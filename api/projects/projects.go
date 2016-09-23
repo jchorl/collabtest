43f22705ff96b43c963ff2f0c0544337e40ed095
Josh Chorlton <jchorlton@gmail.com>
2016-09-23 22:09:27 +0800

    created middleware to pass db pool in context

--- api/projects/projects.go
+++ api/projects/projects.go
@@ -1,10 +1,14 @@
 package projects
 
 import (
+	"errors"
 	"net/http"
 
+	"github.com/Sirupsen/logrus"
+	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
 
+	"github.com/jchorl/collabtest/constants"
 	"github.com/jchorl/collabtest/models"
 )
 
@@ -15,9 +19,12 @@ func Init(projects *echo.Group) {
 
 func create(c echo.Context) error {
 	projectName := c.FormValue("name")
-	db, err := models.GetDB()
-	if err != nil {
-		return err
+	db, ok := c.Get(constants.CTX_DB).(*gorm.DB)
+	if !ok {
+		logrus.WithFields(logrus.Fields{
+			"context": c,
+		}).Error("Unable to get DB from context in project create")
+		return errors.New("Unable to get DB from context")
 	}
 
 	project := models.Project{Name: projectName}
--- server.go
+++ server.go
@@ -1,19 +1,29 @@
 package main
 
 import (
-	"github.com/jchorl/collabtest/api"
-
 	"github.com/Sirupsen/logrus"
+	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
 	"github.com/labstack/echo/middleware"
+
+	"github.com/jchorl/collabtest/api"
+	"github.com/jchorl/collabtest/constants"
+	"github.com/jchorl/collabtest/models"
 )
 
 func main() {
 	logrus.SetFormatter(&logrus.JSONFormatter{})
 
+	db, err := models.GetDB()
+	if err != nil {
+		logrus.WithError(err).Fatal("Could not connect to DB")
+		return
+	}
+
 	e := echo.New()
 	e.Use(middleware.Logger())
+	e.Use(dbMiddleware(db))
 	e.File("/", "static/index.html")
 	e.File("/new", "static/new.html")
 	e.Static("/static", "static")
@@ -22,4 +32,13 @@ func main() {
 	api.Init(apiRoutes)
 
 	e.Run(standard.New(":8080"))
+}
+
+func dbMiddleware(db *gorm.DB) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Set(constants.CTX_DB, db)
+			return next(c)
+		}
+	}
 }
