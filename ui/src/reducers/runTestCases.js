f4c6fe751d88490635f2372fc140c48a91dca6cd
Josh Chorlton <jchorlton@gmail.com>
2016-11-20 21:34:57 +0800

    show testcases

--- ui/src/reducers/runTestCases.js
+++ ui/src/reducers/runTestCases.js
@@ -2,16 +2,13 @@ import Immutable from 'immutable';
 
 import { RUN_TEST_CASES, RUN_TEST_CASES_COMPLETE } from '../actions';
 
-export default function runTestCases(state = Immutable.Map({
-    success: false,
-    lastRun: Immutable.Map()
-}), action) {
-    switch (action.type) {
-        case RUN_TEST_CASES:
-            return state.set('success', false);
-        case RUN_TEST_CASES_COMPLETE:
-            return state.set('success', true).setIn(['lastRun', action.hash], Immutable.fromJS(action.results));
-        default:
-            return state
-    }
+export default function runTestCases(state = Immutable.Map(), action) {
+  switch (action.type) {
+    case RUN_TEST_CASES:
+      return state.setIn([action.hash, 'complete'], false);
+    case RUN_TEST_CASES_COMPLETE:
+      return state.setIn([action.hash, 'complete'], true).setIn([action.hash, 'results'], Immutable.fromJS(action.results));
+    default:
+      return state
+  }
 }
